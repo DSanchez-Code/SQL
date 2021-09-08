cdc88f92d6527464948d24948359853bbdba926d
Dan Sanchez <docksidestrings@gmail.com>
2021-09-07 22:06:52 -0400

    Add files via upload

---
+++ SQL Video 2 - From, Order By.sql
@@ -0,0 +1,18 @@
+-- ORDER BY
+
+USE Northwind
+
+-- 1. Select all from the Products.
+
+SELECT *
+FROM Products
+
+-- 2. Select the ProductID, ProductName, Category ID, and UnitPrice.
+--    Order by ProductID.
+--    Order by ProductName.
+--    Order by UnitPrice, Ascending. Descending
+--    Order first by CategoryID ascending, then by Unit Price descending
+
+SELECT
+FROM P
+
